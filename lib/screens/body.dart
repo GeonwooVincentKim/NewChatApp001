32b3b1d422a37b6a47db03fd365e386e83e323d1
GeonwooVincentKim <kdsnop@gmail.com>
2023-01-09 11:29:47 +0900

    [Feature] - No problem with launching ChatApp and Body

--- lib/screens/body.dart
+++ lib/screens/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:new_ios_chatapp_0/model/user.dart';
 import 'package:new_ios_chatapp_0/provider/user_provider.dart';
+import 'package:new_ios_chatapp_0/screens/chat_app.dart';
 import 'package:new_ios_chatapp_0/widget/list_view/user_card.dart';
 import 'package:provider/provider.dart';
 
@@ -12,45 +13,7 @@ class Body extends StatefulWidget {
 }
 
 class _CupertinoChatState extends State<Body> {
-  List<User> userList = List.empty(growable: true);
-  late TextEditingController _controller;
 
-  // @override
-  void initState() {
-    super.initState();
-
-    /*
-      feature-ios
-      
-      _controller = TextEditingController();
-    */
-    
-    // master Branch
-    userList = UserProvider().filteredUsers;
-    _controller = TextEditingController();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  void _updateUserList(String value) {
-    debugPrint(value);
-
-    if (value.isNotEmpty) {
-      userList  = userList 
-          .where((element) =>
-              element.name.toLowerCase().contains(value.toLowerCase()))
-          .toList();
-    } else {
-      _controller.text = '';
-      userList = userList ;
-    }
-
-    setState(() {});
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -98,22 +61,7 @@ class _CupertinoChatState extends State<Body> {
             );
           
           case 1:
-            return CupertinoPageScaffold(
-              child: CustomScrollView(
-                slivers: [
-                  const CupertinoSliverNavigationBar(
-                    largeTitle: Text('Testing')
-                  ),
-                  SliverGrid(
-                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 1, 
-                      childAspectRatio: 5
-                    ),
-                    delegate: SliverChildBuilderDelegate((context, index) => UserCard(findUser: userList[index])),
-                  )
-                ],
-              )
-            );
+            return ChatApp();
         }
 
         return const CupertinoPageScaffold(
@@ -124,3 +72,4 @@ class _CupertinoChatState extends State<Body> {
     );
   }
 }
+
---
+++ lib/screens/chat_app.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/cupertino.dart';
+import 'package:new_ios_chatapp_0/model/user.dart';
+import 'package:new_ios_chatapp_0/provider/user_provider.dart';
+import 'package:new_ios_chatapp_0/widget/list_view/user_card.dart';
+
+class ChatApp extends StatefulWidget {
+  const ChatApp({
+    super.key,
+  });
+
+
+  @override
+  State<ChatApp> createState() => _ChatAppState();
+}
+
+class _ChatAppState extends State<ChatApp> {
+  List<User> userList = List.empty(growable: true);
+  late TextEditingController _controller;
+
+  // @override
+  void initState() {
+    super.initState();
+
+    /*
+      feature-ios
+      
+      _controller = TextEditingController();
+    */
+    
+    // master Branch
+    userList = UserProvider().filteredUsers;
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _updateUserList(String value) {
+    debugPrint(value);
+
+    if (value.isNotEmpty) {
+      userList  = userList 
+          .where((element) =>
+              element.name.toLowerCase().contains(value.toLowerCase()))
+          .toList();
+    } else {
+      _controller.text = '';
+      userList = userList ;
+    }
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: CustomScrollView(
+        slivers: [
+          const CupertinoSliverNavigationBar(
+            largeTitle: Text('Testing')
+          ),
+          SliverGrid(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1, 
+              childAspectRatio: 5
+            ),
+            delegate: SliverChildBuilderDelegate((context, index) => UserCard(findUser: userList[index])),
+          )
+        ],
+      )
+    );
+  }
+}
